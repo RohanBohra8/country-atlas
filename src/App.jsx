665f68e1985dd357f99ecc3e62052ca371b0b8b8
Rohan Bohra <rohan.bohra@zs.com>
2025-02-28 12:38:16 +0530

    using children props for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import {Home} from "./pages/Home"
-import {About} from "./pages/About"
-import {Contact} from "./pages/Contact"
-import {Country} from "./pages/Country"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import "./App.css"
+import { AppLayout } from "./components/layout/AppLayout";
+
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Country } from "./pages/Country";
+
+import "./App.css"; 
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/country",
+        element: <Country />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
   },
-  {
-    path: "/about",
-    element: <About />
-  },
-  {
-    path: "/country",
-    element: <Country />
-  },
-  {
-    path: "/Contact",
-    element: <Contact />
-  }
 ]);
 
-
 export const App = () => {
-
-
-  
-  return <RouterProvider router={router}/>
-}
-
+  return <RouterProvider router={router} />;
+};
