c500d54b08d268cac0ae089ea08270df46e1b94b
Rohan Bohra <rohan.bohra@zs.com>
2025-02-28 12:20:21 +0530

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import {Home} from "./pages/Home"
+import {About} from "./pages/About"
+import {Contact} from "./pages/Contact"
+import {Country} from "./pages/Country"
+
+import "./App.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/country",
+    element: <Country />
+  },
+  {
+    path: "/Contact",
+    element: <Contact />
+  }
+]);
 
 
 export const App = () => {
+
+
   
-  return (
-    <>
-      
-    </>
-  )
+  return <RouterProvider router={router}/>
 }
 
