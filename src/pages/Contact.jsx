dbd3f61b027b7edc1acb382332b063ee244718d2
Rohan Bohra <rohan.bohra@zs.com>
2025-03-01 12:35:48 +0530

    footer added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
 
 export const Contact = () => {
+  const handleFormSubmit = (formData) => { //formData is given by React19, its just like a new api function 
+    //console.log(formData.entries()); //returns an itrator of key value pairs from the Form Data
+    const formInputData = Object.fromEntries(formData.entries()); //yha se object ki form me input data ko extract kr lia 
+    // console.log(formInputData);
+    
+  }
   return (
-    <h1>Contact</h1>
-  )
-}
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+      <div className="container contact-wrapper">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            name="username"
+            className="form-control"
+            required
+            autoComplete="false"
+            placeholder="Enter your name"
+          />
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            required
+            autoComplete="false"
+            placeholder="Enter your email"
+          />
+          <textarea
+            rows="10"
+            name="message"
+            className="form-control"
+            required
+            autoComplete="false"
+            placeholder="Enter your message"
+          ></textarea>
+
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
