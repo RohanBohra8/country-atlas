ff8d945f58c13a4890aa3a726be93bbb56fa7787
Rohan Bohra <rohan.bohra@zs.com>
2025-03-01 10:35:07 +0530

    added hero section and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from "react";
+import CountryData from "../api/CountryData.json";
 
 export const About = () => {
   return (
-    <h1>About</h1>
-  )
-}
-
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are some Interesting facts
+        <br />
+        We are proud of
+      </h2>
+      <div className="gradient-cards">
+        {CountryData.map((country) => {
+          return (
+            <div className="card" key={country.id}>
+              <div className="container-card bg-orange-box">
+                <p className="card-title">{country.countryName}</p>
+                <p>
+                  <span className="card-description">Capital: </span>
+                  {country.capital}
+                </p>
+                <p>
+                  <span className="card-description">Population: </span>
+                  {country.population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Facts: <br /></span>
+                  {country.interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { HeroSection } from "../components/UI/HeroSection";
+import { About } from "./About";
 
 export const Home = () => {
   return (
-    <h1>Home</h1>
-  )
-}
+    <>
+    <HeroSection />
+    <About /> 
+    </>
+  );
+};
