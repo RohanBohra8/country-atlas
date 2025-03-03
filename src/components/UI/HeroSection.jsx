8f7d013f9df6d5c1250d4de3267cad49c2ea66f2
Rohan Bohra <rohan.bohra@zs.com>
2025-03-03 10:47:45 +0530

    made small changes

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaLongArrowAltRight } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 
 export const HeroSection = () => {
   return (
@@ -7,15 +8,17 @@ export const HeroSection = () => {
           <div className="container grid grid-two-cols">
             <div className="hero-content">
               <h1 className="heading-xl">
-                Explaore the world, One Country at a time.
+                Explore the world, One Country at a time.
               </h1>
               <p className="paragraph">
-                Discover the historr, cutlre and beauty of every nation. search and
+                Discover the history, cutlre and beauty of every nation. search and
                 filter through countries to find the details
               </p>
+              <NavLink to="/country">
               <button className="btn btn-darken btn-inline bg-white-box">
                 start Exploring <FaLongArrowAltRight />
               </button>
+              </NavLink>
             </div>
             <div className="hero-image">
               <img src="/images/world.png" alt="world beauty" className="banner-image" />
